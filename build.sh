038c7287b571b467a769af5fb2e27f4bcc3e77ce
Krixlion <konstanty.cyran.k@gmail.com>
2022-11-20 14:42:00 +0100

    Add continue flag to dlv

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /app
 go build -o main ${BUILD_ARGS} && \
-dlv --listen=:${DEBUG_PORT} --headless=true --log=true --accept-multiclient --api-version=2 exec ./main -- ${RUN_ARGS}
+dlv --listen=:${DEBUG_PORT} --headless=true --log=true --continue --accept-multiclient --api-version=2 exec ./main -- ${RUN_ARGS}
