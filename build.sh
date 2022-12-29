1b17e66c8e11ee94b24c477c5439a414f5269227
Krixlion <konstanty.cyran.k@gmail.com>
2022-12-29 20:54:56 +0100

    Add option to launch container in debug test mode

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-cd /app
-go build -gcflags='all=-N -l' -o main  ${BUILD_ARGS} && \
-dlv --listen=:${DEBUG_PORT} --headless=true --log=true ${DELVE_ARGS} --accept-multiclient --api-version=2 exec ./main -- ${RUN_ARGS}
+cd /app || exit
+
+if [ "$TESTING" == "true" ]; then
+    go build -gcflags='all=-N -l' -o main "${BUILD_ARGS}" &&
+        dlv --listen=:"${DEBUG_PORT}" --headless=true --log=true "${DELVE_ARGS}" --accept-multiclient --api-version=2 test "${TEST_PATH}" -- -test.run '.*'
+else 
+    go build -gcflags='all=-N -l' -o main "${BUILD_ARGS}" &&
+        dlv --listen=:"${DEBUG_PORT}" --headless=true --log=true "${DELVE_ARGS}" --accept-multiclient --api-version=2 exec ./main -- "${RUN_ARGS}"
+fi
